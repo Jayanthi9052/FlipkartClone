1a30cfbefae0e0e1a07f432a741ba868cbe341ed
jayanthi9052 <jayanthireddyg9052@gmail.com>
2025-03-12 19:17:20 +0530

    fix:tailwind css

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
-  plugins: [react()],
-  base: './', // Ensures relative paths for assets
-  optimizeDeps: {
-    include: ['@headlessui/react'],
-  },
+  plugins: [react(), tailwindcss()],
 });
