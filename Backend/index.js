0633a3b4205cd149b086acd0edbfa905c110e6e1
jayanthi9052 <jayanthireddyg9052@gmail.com>
2025-03-12 19:42:48 +0530

    cors:changes in cors

--- Backend/index.js
+++ Backend/index.js
@@ -26,11 +26,7 @@ import searchProductRouter from './Routes/searchProductRouter.js';
 dotenv.config()
 
 const app=express()
-app.use(cors({
-  origin: ["http://localhost:5173", "https://flipkart-clone-c9vy.vercel.app"], // Allow local and deployed frontend
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors())
 app.use(express.json())
 
 app.use(productRouter)
